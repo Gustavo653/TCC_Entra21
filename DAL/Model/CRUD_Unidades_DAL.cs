354e7b24c233c6895979791ef08c4730f37e87cb
Gustavo <gustavohs2004@gmail.com>
2021-09-27 08:43:50 -0300

    CRUD_Unidades pronto

---
+++ DAL/Model/CRUD_Unidades_DAL.cs
@@ -0,0 +1,67 @@
+﻿using DAL.Model.Consultas;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Model
+{
+    public class CRUD_Unidades_DAL
+    {
+        private static readonly SqlConnection conn = DbConnection.conn;
+        public static List<string> GetUnidades()
+        {
+            string select = $"SELECT * from dbo.Unidades";
+            List<string> lista = new List<string>();
+            SqlCommand cmd = new SqlCommand(select, conn);
+            conn.Open();
+            SqlDataReader dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                lista.Add(dr["Nome"].ToString());
+            }
+            dr.Close();
+            conn.Close();
+            return lista;
+        }
+        public static List<string> GetUnidadesPorNome(string nome)
+        {
+            string select = $"SELECT * from dbo.Unidades WHERE Nome LIKE '%{nome}%'";
+            List<string> lista = new List<string>();
+            SqlCommand cmd = new SqlCommand(select, conn);
+            conn.Open();
+            SqlDataReader dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                lista.Add(dr["Nome"].ToString());
+            }
+            dr.Close();
+            conn.Close();
+            return lista;
+        }
+        public static void InsereUnidade(string nome)
+        {
+            string insert = $"INSERT into dbo.Unidades(Nome) values ('{nome}')";
+            DbConnection.Execute(insert);
+        }
+        public static void RemoveUnidade(string nome)
+        {
+            string delete = $"DELETE from dbo.Unidades WHERE Nome = '{nome}'";
+            DbConnection.Execute(delete);
+        }
+        public static void AtualizaUnidade(string nome, string where)
+        {
+            string update = $"UPDATE dbo.Unidades Set Nome = '{nome}' WHERE Nome = '{where}'";
+            DbConnection.Execute(update);
+        }
+        public static bool VerificaSeUnidadeRepete(string nome)
+        {
+            List<string> lista = DbConnection.GenericSelectUnit("Nome", "Unidades");
+            if (lista.Contains(nome))
+                return true;
+            return false;
+        }
+    }
+}
--- FarmaTech/View/Cadastros/TelaCadastroUnidades.cs
+++ FarmaTech/View/Cadastros/TelaCadastroUnidades.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.VisualBasic;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -20,6 +21,7 @@ namespace FarmaTech.View
         private void TelaCadastroUnidades_Load(object sender, EventArgs e)
         {
             tabControl1.TabPages.Remove(tabNovaUnidade);
+            AtualizaDG();
         }
 
         private void TelaCadastroUnidades_FormClosed(object sender, FormClosedEventArgs e)
@@ -43,17 +45,88 @@ namespace FarmaTech.View
 
         private void btnSalvar_Click(object sender, EventArgs e)
         {
-
+            int resultado = BAL.Control.CRUD_Unidades_BAL.AdicionarUnidade(txtNome.Text);            
+            if (resultado == 0)
+            {
+                MessageBox.Show("Unidade cadastrada com sucesso!", "Cadastro", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else if (resultado == 1)
+            {
+                MessageBox.Show("Preencha todos os campos!", "Cadastro", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else if (resultado == 2)
+            {
+                MessageBox.Show("Unidade já existente!", "Cadastro", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else if (resultado == 3)
+            {
+                MessageBox.Show("Houve um erro desconhecido!", "Cadastro", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else if (resultado == 4)
+            {
+                MessageBox.Show("Verifique se os dados inseridos estão no formato correto!", "Cadastro", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            AtualizaDG();
         }
 
         private void btnAlterar_Click(object sender, EventArgs e)
         {
-
+            int indiceSelecionado = dgUnidades.CurrentRow.Index;
+            string nome = Interaction.InputBox("Insira o nome", "Atualiza Usuario", "", 200, 200);
+            int resultado = BAL.Control.CRUD_Unidades_BAL.AtualizaUnidade(nome, dgUnidades.Rows[indiceSelecionado].Cells[0].Value.ToString());
+            if (resultado == 0)
+            {
+                MessageBox.Show("Usuário atualizado com sucesso!", "Atualizar", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else if (resultado == 1)
+            {
+                MessageBox.Show("Preencha todos os campos!", "Atualizar", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else if (resultado == 2)
+            {
+                MessageBox.Show("Usuário já existente!", "Atualizar", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else if (resultado == 3)
+            {
+                MessageBox.Show("Houve um erro desconhecido!", "Atualizar", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else if (resultado == 4)
+            {
+                MessageBox.Show("Verifique se os dados inseridos estão no formato correto!", "Atualizar", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            AtualizaDG();
         }
 
         private void btnExcluir_Click(object sender, EventArgs e)
         {
-
+            int indiceSelecionado = dgUnidades.CurrentRow.Index;
+            BAL.Control.CRUD_Unidades_BAL.RemoveUnidade(dgUnidades.Rows[indiceSelecionado].Cells[0].Value.ToString());
+            AtualizaDG();
+        }
+        public void AtualizaDG()
+        {
+            int tamanhoDG = dgUnidades.Rows.Count;
+            while(tamanhoDG > 0)
+            {
+                dgUnidades.Rows.RemoveAt(0);
+                tamanhoDG--;
+            }
+            if (!string.IsNullOrEmpty(txtPesquisaUsuario.Text))
+            {
+                List<string> lista = BAL.Control.CRUD_Unidades_BAL.GetUnidadesPorNome(txtPesquisaUsuario.Text);
+                foreach (var item in lista)
+                {
+                    dgUnidades.Rows.Add(item);
+                }
+            }
+            else
+            {
+                List<string> lista = BAL.Control.CRUD_Unidades_BAL.GetUnidades();
+                foreach (var item in lista)
+                {
+                    dgUnidades.Rows.Add(item);
+                }
+            }
         }
     }
 }
