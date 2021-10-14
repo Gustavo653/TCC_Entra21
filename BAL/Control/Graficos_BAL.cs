ea4d97757db39b999f9fef323a4f32aaead30599
Gustavo <gustavohs2004@gmail.com>
2021-10-14 10:39:11 -0300

    HotFix Venda

---
+++ BAL/Control/Graficos_BAL.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BAL.Control
+{
+    public class Graficos_BAL
+    {
+        public static void LucroPorFuncionario()
+        {
+
+        }
+        public static void FuncionarioComMaisVendas()
+        {
+
+        }
+        public static void ProdutosMaisVendidos()
+        {
+
+        }
+    }
+}
