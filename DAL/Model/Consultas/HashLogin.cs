cff1f9731bc9886c2e65c271d79cc527099b0f3d
Gustavo <gustavohs2004@gmail.com>
2021-10-16 15:44:38 -0300

    Removendo criptografia

--- DAL/Model/Consultas/HashLogin.cs
+++ DAL/Model/Consultas/HashLogin.cs
@@ -16,29 +16,12 @@ namespace DAL.Model.Consultas
         }
         public string CriptografarSenha(string senha)
         {
-            var encodedValue = Encoding.UTF8.GetBytes(senha); //Codificação de caracteres em UTF8
-            var encryptedPassword = _algoritmo.ComputeHash(encodedValue); //Calcula o hash dos caracteres UTF8
-
-            var sb = new StringBuilder();
-            foreach (var caracter in encryptedPassword)
-            {
-                sb.Append(caracter.ToString("X2")); //Determina que a senha deve ser hexadecimal
-            }
-
-            return sb.ToString();
+            return senha;
         }
 
         public bool VerificarSenha(string senhaDigitada, string senhaCadastrada)
         {
-            var encryptedPassword = _algoritmo.ComputeHash(Encoding.UTF8.GetBytes(senhaDigitada)); //Recebe a senha inserida pelo usuario e converte para hash
-
-            var sb = new StringBuilder();
-            foreach (var caractere in encryptedPassword)
-            {
-                sb.Append(caractere.ToString("X2")); //Determina que a senha deve ser hexadecimal
-            }
-
-            return sb.ToString() == senhaCadastrada; //Se a senha for a mesma, retorna true
+            return senhaDigitada == senhaCadastrada;
         }
     }
 }
